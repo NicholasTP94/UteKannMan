6f8ab4897e988e9d98ff79918823b793aec5c4f6
JohnnyToMpratsoni <13-13401@saeinstitute.edu>
2024-02-11 16:48:29 +0200

    PAUSE SCRIPT + NEW SCENE

---
+++ Assets/Assets/UI/Pause_menu_ui/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pauseMenuUI; // Assign in Inspector
+    public bool isGamePaused = false;
+
+    void Update()
+    {
+        // Check if the pause button was pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Resume the game
+        isGamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Pause the game
+        isGamePaused = true;
+        LoadNewScene("Controls");
+    }
+
+    public void LoadNewScene(string Controls)
+    {
+        SceneManager.LoadScene(Controls);
+    }
+
+}
